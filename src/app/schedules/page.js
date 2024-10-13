53d0acad51f2af7da9602927e82b4bf04af98814
PauloAlmeidaBrasa <almeida.desenvolve@gmail.com>
2024-10-12 21:47:50 -0300

    feat: call new component to list

--- src/app/schedules/page.js
+++ src/app/schedules/page.js
@@ -2,8 +2,9 @@
 
 import React from 'react';
 
+import ScheduleTable from './list';
+
 async function fetchSchedules() {
-    console.log(`${process.env.NEXT_PUBLIC_API_URL}/scheduling`)
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/scheduling`, {
     next: { revalidate: 10 }, // Revalidate every 10 seconds
   });
@@ -13,7 +14,6 @@ async function fetchSchedules() {
   if (!response.ok) {
     throw new Error('Failed to fetch schedules');
   }
-
   return  result.data;
 }
 
@@ -23,16 +23,7 @@ const SchedulesPage = async () => {
   return (
     <div>
       <h1>Schedules</h1>
-      <ul>
-        {schedules.map((schedule) => (
-          <li key={schedule.id}>
-            <p>Date Start: {new Date(schedule.date_start).toLocaleString()}</p>
-            <p>Date End: {new Date(schedule.date_end).toLocaleString()}</p>
-            <p>Available: {schedule.available ? 'Yes' : 'No'}</p>
-            <p>User ID: {schedule.user_id}</p>
-          </li>
-        ))}
-      </ul>
+       <ScheduleTable schedules={schedules} />
     </div>
   );
 };
